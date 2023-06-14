4082ee993f72203fe9b886ca9a7e6b436a0467bb
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-06-14 06:05:56 +0530

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -148,6 +148,54 @@ void sorting(node* head){
     }
 }
 
+node* merge(node* &head1, node* &head2){
+    node* p1=head1;
+    node* p2=head2;
+    node* dummyNode=new node(-1);
+    node*p3=dummyNode;
+    while(p1!=NULL && p2!=NULL){
+        if(p1->data<p2->data){
+            p3->next=p1;
+            p1=p1->next;
+        }
+        else{
+            p3->next=p2;
+            p2=p2->next;
+        }
+        p3=p3->next;
+    }
+    while(p1!=NULL){
+        p3->next=p1;
+        p1=p1->next;
+        p3=p3->next;
+    }
+    while(p2!=NULL){
+        p3->next=p2;
+        p2=p2->next;
+        p3=p3->next;
+    }
+    return dummyNode->next;
+}
+
+node* mergeRecursive(node* &head1, node* &head2){
+    if(head1==NULL){
+        return head2;
+    }
+    if(head2==NULL){
+        return head1;
+    }
+    node* result;
+    if(head1->data<head2->data){
+        result=head1;
+        result->next=mergeRecursive(head1->next,head2);
+    }
+    else{
+        result=head2;
+        result->next=mergeRecursive(head1,head2->next);
+    }
+    return result;
+}
+
 int main()
 {
     node* head=NULL;
@@ -171,7 +219,12 @@ int main()
     //node* newhead=reversek(head,k);
     //display(newhead);
     //cout<<getCount(head)<<endl;
-    sorting(head);
-    display(head);
+    //sorting(head);
+    //display(head);
+    display(head1);
+    display(head2);
+    node* newhead=merge(head1,head2);
+    node* newhead=mergeRecursive(head1,head2);
+    display(newhead);
     return 0;
 }
