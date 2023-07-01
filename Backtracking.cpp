7f9526974f99b84cf9a559a4b3320f30541302a5
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-01 14:10:13 +0530

    Update Backtracking.cpp

--- Backtracking.cpp
+++ Backtracking.cpp
@@ -63,3 +63,78 @@ int main()
   }
   return 0;
 }
+
+N Queen problem
+//permutation is a rearrangement of members of a sequence into a new seq
+#include <iostream>
+using namespace std;
+bool isSafe(int** arr, int x, int y, int n){
+    for(int row=0;row<x;row++){
+        if(arr[row][y]==1){
+            return false;
+        }
+    }
+    
+    int row=x;
+    int col=y;
+    while(row>=0 && col>=0){
+        if(arr[row][col]==1){
+            return false;
+        }
+        row--;
+        col--;
+    }
+    
+    row=x;
+    col=y;
+    while(row>=0 && col<n){
+        if(arr[row][col]==1){
+            return false;
+        }
+        row--;
+        col++;
+    }
+    return true;
+}
+bool nQueen(int** arr,int x,int n){ 
+    //y is not taken into consideration cuz once we place a queen in a row the next queen automatically shits to the next row as there can be only one in a row
+    if(x>=n){
+        return true;
+    }
+    for(int col=0;col<n;col++){
+        if(isSafe(arr,x,col,n)){
+            arr[x][col]=1;
+            if(nQueen(arr,x+1,n)){
+                return true;
+            }
+            arr[x][col]=0; //backtracking
+        }
+    }
+    return false;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int** arr=new int*[n];
+    for(int i=0;i<n;i++){
+        arr[i]=new int[n];
+        for(int j=0;j<n;j++){
+            arr[i][j]=0;
+        }
+    }
+    if(nQueen(arr,0,n)){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
+        }
+    }
+    return 0;
+}
+//i/p: 4
+ans:
+0 1 0 0
+0 0 0 1
+1 0 0 0
+0 0 1 0
