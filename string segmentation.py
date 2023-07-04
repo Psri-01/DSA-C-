b189c884ec549269a35665d2756a1df55bfefb83
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-04 23:34:21 +0530

    Update string segmentation.py

--- string segmentation.py
+++ string segmentation.py
@@ -13,3 +13,13 @@ if can_segment_string(s, dictionary):
   print("String Can be Segmented")
 else:
   print("String Can NOT be Segmented")
+  
+'''
+ALGO:
+n = length of input string
+for i = 0 to n - 1
+  first_word = substring (input string from index [0, i] )
+  second_word = substring (input string from index [i + 1, n - 1] )
+  if dictionary has first_word
+    if second_word is in dictionary OR second_word is of zero length, then return true
+    recursively call this method with second_word as input and return true if it can be segmented'''
