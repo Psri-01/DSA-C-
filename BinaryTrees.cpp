22258a93b4ea3e982574f029f5cdd76776c85831
Padmasri Srinivas <114862496+Psri-01@users.noreply.github.com>
2023-08-18 19:43:49 +0530

    Update BinaryTrees.cpp

--- BinaryTrees.cpp
+++ BinaryTrees.cpp
@@ -1,4 +1,5 @@
 //BINARY TREE
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -54,6 +55,34 @@ int sumNodes(Node* root){
     return sumNodes(root->left)+sumNodes(root->right)+root->data;
 }
 
+int height(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int lh=height(root->left);
+    int rh=height(root->right);
+    return max(lh,rh)+1;  //+1 cuz current node
+}
+
+bool isBalanced(Node* root){
+    if(root==NULL){
+        return true;
+    }
+    if(isBalanced(root->left)==false){
+        return false;
+    }
+    if(isBalanced(root->right)==false){
+        return false;
+    }
+    int lh=height(root->left);
+    int rh=height(root->right);
+    if(abs(lh-rh)<=1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 int main()
 {
     struct Node* root=new Node(1);
@@ -63,14 +92,29 @@ int main()
     root->left->right=new Node(5);
     root->right->left=new Node(6);
     root->right->right=new Node(7);
+    
+    Node* root2=new Node(1);
+    root2->left=new Node(2);
+    root2->left->left=new Node(3);
     /*cout<<"preorder traversal: ";
     preorder(root);
     cout<<"inorder traversal: ";
     inorder(root);
     cout<<"postorder traversal: ";
     postorder(root);*/
-    cout<<countNodes(root)<<endl;
-    cout<<sumNodes(root)<<endl;
+    //cout<<countNodes(root)<<endl;
+    //cout<<sumNodes(root)<<endl;
+    cout<<height(root)<<" \n";
+    if(isBalanced(root)){
+        cout<<"Balanced tree"<<endl;
+    } else {
+        cout<<"Unbalanced tree"<<endl;
+    }
+    if(isBalanced(root2)){
+        cout<<"Balanced tree"<<endl;
+    } else {
+        cout<<"Unbalanced tree"<<endl;
+    }
     return 0;
 }
 
